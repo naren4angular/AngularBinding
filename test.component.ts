c28b63a894714ad3bacc4bcf57a07386f0bed862
naren4angular <60656408+naren4angular@users.noreply.github.com>
2020-02-04 16:53:11 -0500

    Add files via upload
    EventHandling Commits

---
+++ test.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { getCurrencySymbol } from '@angular/common';
+
+@Component({
+  selector: 'app-test',
+  template: `
+    <div>
+      <h2>
+      Welcome {{name }}
+      </h2>
+      <h2 [style.color]="'orange'">Style Binding</h2>
+      <h2 [style.color] ="hasError ? 'red' : 'green'">Style Binding Conditions</h2>
+      <h2 [style.color]="highlightColor">Style Binding TWO </h2>
+      <h2 [ngStyle]="titleStyles">Style Binding DEMO</h2>
+      <button (click) ="onClick($event)">Greet</button>
+      <button (click)="greeting ='Welcome Narendra'">Greet</button>
+      {{greeting}}
+
+
+  `,
+  styles: [`
+    .text-success {
+      color: green ;
+    }
+    .text-danger {
+      color: red ;
+     }
+     .text-special {
+       font-style: italic;
+     }
+  `]
+})
+export class TestComponent implements OnInit {
+  public name = "Narendra Reddy";
+  public successClass ="text-success";
+  public hasError  = false;
+  public isSpecial = true;
+  public highlightColor = "yellow";
+  public greeting ="";
+  public titleStyles = {
+    color: "blue",
+    fontStyle: "italic"
+  }
+
+  public messageClasses = {
+    "text-success" : !this.hasError,
+     "text-danger" : this.hasError,
+     "text-special" : this.isSpecial
+  }
+  
+  constructor() { }
+
+  onClick(event) {
+    console.log("Welcome Narendra to Angular Development")
+    console.log(event);
+    //this.greeting ="Angular Projects" ;
+    this.greeting= event.type;
+    
+  }
+
+  ngOnInit() {
+  }
+  userGreetings(){
+    return "Learning Angular" +this.name;
+  }
+
+}
